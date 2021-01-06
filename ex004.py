f6ff8a094bbdc8aaa8653bb13f7e5cd6b38c4372
thiago5171 <tgazaroli@gmail.com>
2021-01-05 23:13:07 -0200

    Create ex004.py

---
+++ ex004.py
@@ -0,0 +1,4 @@
+#crie um algoritimo que leia um  numero e mostre  o seu dobro , triplo, e raiiz quadrada.
+n = float(input("digite um  numero: "))
+n1 = n**(1/2)
+print("o dobro do numer eh {}, o seu triplo é {} , e sua raiz eh {} ".format(n*2,n*3,n1))

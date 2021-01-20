347a03aa4f6a85b1cef4d976eebe1c39466eb710
thiago5171 <tgazaroli@gmail.com>
2021-01-20 15:51:22 -0200

    Update ex014.py

--- ex014.py
+++ ex014.py
@@ -1,4 +1,10 @@
-# Faça um algoritmo que leia o salário de um funcionário e mostre seu novo salário, com 15% de aumento.
-salario = float(input("digite o valor  de seu salario: "))
-aumento = (salario*115)/100
-print("o seu salario de R${} com aumento de 15% ficou R${:.2f}".format(salario,aumento))
+#Escreva um programa que leia a velocidade de um carro. Se ele ultrapassar 80Km/h, mostre 
+#uma mensagem dizendo que ele foi multado. A multa vai custar R$7,00 por cada Km acima do limite
+velocidad = float(input("digite a velocidade que o carro esta: "))
+
+if velocidad >80 :
+    velocidad = velocidad-80
+    multa = velocidad *7
+    print(" voce  ecxedeu o limite de velocidade em {}km/h, o valor  da multa é de R${} .".format(velocidad,multa))
+else:
+    print("voce esta dentro do limite de velovidade.")

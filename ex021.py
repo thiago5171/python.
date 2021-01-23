749090f7543269a2ee5334688d58a73f569e7747
thiago5171 <tgazaroli@gmail.com>
2021-01-23 21:57:44 -0200

    Update ex021.py

--- ex021.py
+++ ex021.py
@@ -1,5 +1,5 @@
 """Escreva um programa para aprovar o empréstimo bancário para a compra de uma casa. Pergunte o valor
- da casa, o salário do comprador e em quantos anos ele vai pagar. A prestação mensal não pode 
+da casa, o salário do comprador e em quantos anos ele vai pagar. A prestação mensal não pode 
 exceder 30% do salário ou então o empréstimo será negado.
 """
 salario = float(input("digite o valor do seu salario: "))

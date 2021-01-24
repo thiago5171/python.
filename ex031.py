2a7ae6f7577c90d76c31259b9c013bf407ededef
thiago5171 <tgazaroli@gmail.com>
2021-01-23 22:14:31 -0200

    Update ex031.py

---
+++ ex031.py
@@ -0,0 +1,6 @@
+#Crie um programa que mostre na tela todos os números pares que estão no intervalo entre 1 e 50.
+
+print("os numeros pares que estão no intervalo entre 1 e 50.")
+for a in range(1,51):
+    if (a%2==0):
+        print(a)

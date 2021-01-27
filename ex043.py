9e1a50ae4c64162e64437b1fa03aa6da65502761
thiago5171 <tgazaroli@gmail.com>
2021-01-27 02:21:04 -0200

    Update ex043.py

--- ex043.py
+++ ex043.py
@@ -5,11 +5,20 @@ Ex: 5! = 5 x 4 x 3 x 2 x 1 = 120"""
 num = int(input(" digite um numero para saber seu fatorial: "))
 print("num")
 cont = num
+
 fatorial = 1
-print("{}! = ".format(num), end=' ')
+
+print("{}! = ".format(num,num), end=' ')
+
 while cont != 0:
-    print("{} x".format(cont), end=' ')
-    fatorial = fatorial* cont
+#é necessario esse if para que  o x de multiplicacao so apareça apos o primeiro valor do fatorial aparecer
+# e nao apareça algo do tipo x7x8 = 56
+    if cont == num:
+        print("{}".format(cont), end=' ')
+    else:
+        print("x {}".format(cont), end=' ')
+
+    fatorial = fatorial * cont
     cont =  cont -1
 
 

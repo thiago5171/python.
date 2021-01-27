d27bbe7b27e1a5221c38c7cb744745278df27458
thiago5171 <tgazaroli@gmail.com>
2021-01-27 02:10:59 -0200

    Update ex043.py

--- ex043.py
+++ ex043.py
@@ -3,8 +3,18 @@
 Ex: 5! = 5 x 4 x 3 x 2 x 1 = 120"""
 
 num = int(input(" digite um numero para saber seu fatorial: "))
+print("num")
+cont = num
+fatorial = 1
+print("{}! = ".format(num), end=' ')
+while cont != 0:
+    print("{} x".format(cont), end=' ')
+    fatorial = fatorial* cont
+    cont =  cont -1
 
-while cont
+
+print("= {}".format(fatorial))
+
 
 
 

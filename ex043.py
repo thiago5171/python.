36a5ab89ba9c523ec51dbb6b10040874b5d9f81a
thiago5171 <tgazaroli@gmail.com>
2021-01-27 01:54:47 -0200

    Update ex043.py

--- ex043.py
+++ ex043.py
@@ -1,5 +1,10 @@
+"""Faça um programa que leia um número qualquer e mostre o seu fatorial.
 
+Ex: 5! = 5 x 4 x 3 x 2 x 1 = 120"""
 
+num = int(input(" digite um numero para saber seu fatorial: "))
+
+while cont
 
 
 

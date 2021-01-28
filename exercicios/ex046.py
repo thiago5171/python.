d1ddeb59b855ea1681f7bdb21ac6f578e20d5883
thiago5171 <tgazaroli@gmail.com>
2021-01-28 05:28:21 -0200

    Update ex046.py

--- exercicios/ex046.py
+++ exercicios/ex046.py
@@ -1,3 +1,7 @@
+""" Escreva um programa que leia um número N inteiro qualquer e mostre na tela os N primeiros elementos de uma Sequência de Fibonacci. 
+
+Ex: 0 - 1 - 1 - 2 - 3 - 5 - 8
+"""
 n = int(input("digite ate que que termo a sequencia de fibonacci deseja saber: "))
 cont=1
 cont2=1

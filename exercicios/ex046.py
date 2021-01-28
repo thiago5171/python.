0675660cf6503b87c7fce99a3abdf8ea8b0a93f7
thiago5171 <tgazaroli@gmail.com>
2021-01-28 05:12:36 -0200

    Create ex046.py

---
+++ exercicios/ex046.py
@@ -0,0 +1,19 @@
+n = int(input("digite ate que que termo a sequencia de fibonacci deseja saber: "))
+cont=1
+cont2=1
+
+aux2=1
+aux1=0
+print("0->  ", end=' ')
+while cont <= n/2:
+   
+    aux3 = aux1+aux2
+    n1=aux3
+    aux3=n1 +aux2
+    print("{}-> {}->".format(n1,aux3), end=' ')
+        
+    aux1 = n1
+    aux2 = aux3
+
+    cont +=1
+        

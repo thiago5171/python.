6b110f4cf8fef4bf4468b9f977563f55cb658f6d
thiago5171 <tgazaroli@gmail.com>
2021-02-21 03:42:40 -0300

    Update ex1.py

--- exerc_da_faculdade/ex1.py
+++ exerc_da_faculdade/ex1.py
@@ -2,7 +2,7 @@
 # e a porcentagem do aumento. Exiba o valor do aumento e do novo salário.
 salario = float(input ("digite o valor do salário: "))
 aumento = float (input ("digite a porcentagem do aumenta do salário: \n"))
-
+#abaixo eu calculo o valor do aumento, somando o 100 que esta multiplicnado com o valor da porcentagem inputado para calcular o valor total
 porcent= (salario*(100+aumento))/100
 
 print(f"o salário que era de R${salario} após o aumento de {aumento}% o salário ficou de R${porcent}")

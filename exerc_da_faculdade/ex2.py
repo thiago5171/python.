fb61b933b94933a024f2b008814065a8ff8c7121
thiago5171 <tgazaroli@gmail.com>
2021-02-21 03:32:48 -0300

    Create ex2.py

---
+++ exerc_da_faculdade/ex2.py
@@ -0,0 +1,10 @@
+""" Desenvolva em cada LP um programa que solicite o preço de uma mercadoria e o percentual de desconto.
+ Exiba o valor do desconto e o preço a pagar."""
+ 
+Preço= Float (input ("digite o valor: "))
+Desconto= Float (input ("digite a porcentagem do desconto da mercadoria: "))
+
+descontado = ( preço* desconto)/100
+Pagar= preço- descontando
+
+Print (f"o valor do desconto foi de R${descontado}, e o preço total a pagar do produto foi de  R${Pagar}")

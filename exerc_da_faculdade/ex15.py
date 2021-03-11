0b7848a6d795197690db95046910b15d881d01f9
thiago5171 <Tgazaroli@gmail.com>
2021-03-11 18:54:34 -0300

    Update ex15.py

--- exerc_da_faculdade/ex15.py
+++ exerc_da_faculdade/ex15.py
@@ -1,4 +1,4 @@
-n= str(input("digite uma frase/palavra ")).upper().strip()
+n= str(input("digite um valor ")).upper().strip()
 d=n.split()
 j= ''.join(d)
 inver= ''
@@ -6,7 +6,7 @@ for i in range(len(j)-1,-1,-1):
     inver+=j[i]
 
 if n==inver:
-    print("a frase é um palindromo")
+    print("o valor é um palindromo")
 else:
-    print("a frase não é um palindromo")
+    print("o valor não é um palindromo")
 

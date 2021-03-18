918861aec4fac116bef6b3a7a3ef48d61b621ecd
thiago5171 <Tgazaroli@gmail.com>
2021-03-18 17:09:18 -0300

    Create ex19.py

---
+++ exerc_da_faculdade/ex19.py
@@ -0,0 +1,3 @@
+""". Faça um programa que leia vários conjuntos de três valores reais e mostre para cada
+conjunto: sua soma, seu produto e sua média. O programa para quando um conjunto não
+entrar com seus valores em ordem crescente."""

a7da3b1b70f048f2234177a532b000df8b2cca33
thiago5171 <Tgazaroli@gmail.com>
2021-04-13 21:51:44 -0300

    Update ex32.py

--- exerc_da_faculdade/ex32.py
+++ exerc_da_faculdade/ex32.py
@@ -1,7 +1,3 @@
-""" A lista de temperaturas de Mons, na Bélgica, foi armazenada na lista T = [-10, -8, 0, 1, 2, 5, -2, -4]. Mostre a menor e a maior temperatura, assim como 
-a temperatura média. A lista de temperaturas de Mons, na Bélgica, foi armazenada na 
-lista T = [-10, -8, 0, 1, 2, 5, -2, -4]. Mostre a menor e a maior temperatura, assim como a temperatura média.
- """
 t = [-10, -8, 0, 1, 2, 5, -2, -4]
 maior= menor= soma = media = cont = n = 0
 
@@ -9,13 +5,11 @@ for i in range(0,len(t),1):
     if i==0 :
         maior=t[i]
         menor=t[i]
-    
-  
-    if (t[i]) < menor:
-        menor=t[i]
-
-    if t[i]>maior:
-        menor=t[i]
+    else:
+        if t[i] < menor:
+            menor=t[i]
+        if t[i] > maior :
+          maior=t[i]
 
 for i in t:
     soma = soma + i
@@ -23,6 +17,6 @@ for i in t:
 media = soma/cont
 
 
-print(f"maior temperatura->{maior}\n",
+print(f" maior temperatura->{maior}\n",
 f"menor temperatura->{menor}\n"
-f"tempreatura media->{media:.2f}" )     
+f" tempreatura media->{media:}" )     
